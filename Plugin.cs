fa61526b6961cfbfaf1cb3e823fea80e5bc4f431
David <david.ferreira.pires@everis.com>
2018-03-26 21:35:37 +0100

    Initial

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Subble.Core;
+using Subble.Core.Logger;
+using Subble.Core.Plugin;
+
+namespace ConfigManager
+{
+    public class Plugin : ISubblePlugin
+    {
+        public IPluginInfo Info
+            => new PluginInfo();
+
+        public SemVersion Version
+            => new SemVersion(0, 0, 1);
+
+        public long LoadPriority => 5;
+
+        public IEnumerable<Dependency> Dependencies
+            => new[]
+            {
+                new Dependency(typeof(ILogger), 0, 0, 1)
+            };
+
+
+        public bool Initialize(ISubbleHost host)
+        {
+            return true;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using Subble.Core.Plugin;
+using System;
+
+namespace ConfigManager
+{
+    public class PluginInfo : IPluginInfo
+    {
+        public string GUID => "8a26b5f8-3d25-440c-8da9-c3f0bed34232";
+
+        public string Name => "ConfigationManager";
+
+        public string Creator => "David Pires";
+
+        public string Repository => "https://github.com/Subble/ConfigManager";
+
+        public string Support => "https://github.com/Subble/ConfigManager/issues";
+
+        public string Licence => "MIT";
+    }
+}
