8abcddf8fff1b5bf752519c6f83e39a22bcb80f7
David Pires <davidblkx@gmail.com>
2018-04-06 19:01:51 +0100

    Added tests

---
+++ Tests/ConfigTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Subble.Core;
+using Subble.Core.Config;
+using Subble.Core.Logger;
+using System.IO;
+using static Subble.Core.Func.Option;
+
+namespace ConfigManager.Tests
+{
+    [TestClass]
+    public class ConfigTest
+    {
+        private ISubbleHost GetHostMock()
+        {
+            var mock = new Mock<ISubbleHost>();
+
+            return mock.Object;
+        }
+
+        [TestMethod]
+        public void TestConfigInit()
+        {
+            var host = GetHostMock();
+            var folderName = "TestConfigManager";
+
+            var config = new Config("TestConfigManager", host, None<ILogger>());
+
+            Assert.IsNotNull(config);
+            Assert.IsInstanceOfType(config, typeof(IConfigManager));
+
+            const string stringKey = "test.string";
+            const string stringValue = "StringValue1ç";
+            config.Set(stringKey, stringValue);
+
+
+
+            var testStringValue = config.Get<string>(stringKey);
+            Assert.AreEqual(stringValue, testStringValue);
+        }
+
+    }
+}
